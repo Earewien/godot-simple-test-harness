6884c63edcfb96de694b948b8fe281c4035629fa
Adrien Quillet <adrien.quillet@icloud.com>
2023-02-07 18:02:32 +0100

    Fix #6 : add test case template

---
+++ script_templates/TestCase/default.gd
@@ -0,0 +1,28 @@
+extends _BASE_
+
+#------------------------------------------
+# Test setup
+#------------------------------------------
+
+# Called once per test case, before any test
+static func beforeAll() -> void:
+    pass
+
+# Called once per test case, after any test
+static func afterAll() -> void:
+    pass
+
+# Called before each test
+func beforeEach() -> void:
+    pass
+
+# Called after each test
+func afterEach() -> void:
+    pass
+
+#------------------------------------------
+# Tests
+#------------------------------------------
+
+func test() -> void:
+    pass
