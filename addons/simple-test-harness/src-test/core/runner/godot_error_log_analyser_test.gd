5dd54aa80747244a6bf813e553e7141ea7e048fb
Adrien Quillet <adrien.quillet@icloud.com>
2023-02-07 17:34:24 +0100

    Rename test case

--- addons/simple-test-harness/src-test/core/runner/godot_error_log_analyser_test.gd
+++ addons/simple-test-harness/src-test/core/runner/godot_error_log_analyser_test.gd
@@ -73,7 +73,7 @@ func test_error_descriptor_on_user_script_error() -> void:
     assert_equals("User script error: Invalid operands 'Array' and 'String' in operator '+'.", descriptor.error_description)
     assert_equals(2, descriptor.error_line_number)
 
-func test_push_error() -> void:
+func test_error_descriptor_on_push_error() -> void:
     var logs:PackedStringArray = [
         "ERROR: This message is displayed as this when using 'push_error'",
         "   at: push_error (core/variant/variant_utility.cpp:880)"
@@ -83,7 +83,7 @@ func test_push_error() -> void:
     assert_equals("push_error: This message is displayed as this when using 'push_error'", descriptor.error_description)
     assert_equals(-1, descriptor.error_line_number)
 
-func test_assert() -> void:
+func test_error_descriptor_on_assert() -> void:
     var logs:PackedStringArray = [
         "SCRIPT ERROR: Assertion failed: this is an assertion description",
         "          at: test_assert (res://addons/simple-test-harness/src-test/core/runner/godot_error_log_analyser_test.gd:66)"
