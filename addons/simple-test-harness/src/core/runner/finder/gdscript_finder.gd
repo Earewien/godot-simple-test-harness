27d48a82bf7bca8dd5fdc484cf43b43d02b5e443
Adrien Quillet <adrien.quillet@icloud.com>
2023-02-14 17:06:43 +0100

    GDScript Finder: allow to exclude some path from lookup ; by default exclude project template directory

--- addons/simple-test-harness/src/core/runner/finder/gdscript_finder.gd
+++ addons/simple-test-harness/src/core/runner/finder/gdscript_finder.gd
@@ -16,6 +16,9 @@ extends RefCounted
 # Where to look for scripts. Can be directories or file paths. All directories will be scanned recursively
 var lookup_paths:PackedStringArray = []
 
+# Where to NOT look at !
+var excluded_lookup_paths:PackedStringArray = []
+
 #------------------------------------------
 # Variables privées
 #------------------------------------------
@@ -23,6 +26,12 @@ var lookup_paths:PackedStringArray = []
 #------------------------------------------
 # Fonctions Godot redéfinies
 #------------------------------------------
+
+func _init(exclude_template_dir_from_lookup_paths:bool = true) -> void:
+    if exclude_template_dir_from_lookup_paths:
+        var setting_template_dir:String = ProjectSettings.get_setting("editor/script/templates_search_path", "")
+        if not setting_template_dir.is_empty():
+            excluded_lookup_paths.append(setting_template_dir.simplify_path())
 
 #------------------------------------------
 # Fonctions publiques
@@ -43,19 +52,21 @@ func _recursive_find_gd_scripts(path:String) -> Array[GDScript]:
     path = path.simplify_path()
     var scripts:Array[GDScript] = []
 
-    # Recursive search in directory
-    if DirAccess.dir_exists_absolute(path):
-        var res_dir:DirAccess = DirAccess.open(path)
-        res_dir.list_dir_begin()
-        var file_name = res_dir.get_next()
-        while file_name != "":
-            scripts.append_array(_recursive_find_gd_scripts(path.path_join(file_name)))
-            file_name = res_dir.get_next()
-    # Path represents a file !
-    elif FileAccess.file_exists(path):
-        # A file, maybe a GDScript ?
-        if path.get_extension() == "gd":
-            var loaded_file = GDScriptFactory.get_gdscript(path)
-            scripts.append(loaded_file)
+    # Check for excludes!
+    if not excluded_lookup_paths.has(path):
+        # Recursive search in directory
+        if DirAccess.dir_exists_absolute(path):
+            var res_dir:DirAccess = DirAccess.open(path)
+            res_dir.list_dir_begin()
+            var file_name = res_dir.get_next()
+            while file_name != "":
+                scripts.append_array(_recursive_find_gd_scripts(path.path_join(file_name)))
+                file_name = res_dir.get_next()
+        # Path represents a file !
+        elif FileAccess.file_exists(path):
+            # A file, maybe a GDScript ?
+            if path.get_extension() == "gd":
+                var loaded_file = GDScriptFactory.get_gdscript(path)
+                scripts.append(loaded_file)
 
     return scripts
