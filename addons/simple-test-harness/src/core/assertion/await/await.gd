715f4a4de8679ac01c160f159bd29425aaecfc93
Adrien Quillet <adrien.quillet@icloud.com>
2023-02-21 20:10:27 +0100

    Fix #17 : await_for internal change to fix regression due to Godot 4 RC3
    Don't really know why it is broken... but, it's fixed now :/
    Still working on Godot 4 RC2

--- addons/simple-test-harness/src/core/assertion/await/await.gd
+++ addons/simple-test-harness/src/core/assertion/await/await.gd
@@ -5,6 +5,8 @@ extends RefCounted
 # Signaux
 #------------------------------------------
 
+signal _on_until_condition(result:Array)
+
 #------------------------------------------
 # Exports
 #------------------------------------------
@@ -24,8 +26,7 @@ var _at_most_duration_seconds:float = 5 # 5 seconds by default
 var _at_least_duration_seconds:float = 0 # 0 seconds by default
 var _poll_delay_seconds:float = 0.05
 
-var _current_tested_signal_received:bool = false
-var _current_tested_signal_arguments:Array
+var _timer:Timer
 
 #------------------------------------------
 # Fonctions Godot redéfinies
@@ -57,12 +58,21 @@ func until(predicate:Callable) -> void:
     _reporter.assertion_reports.append(await_fail_report)
 
     var start_tick_ms:int = Time.get_ticks_msec()
-    var ellapsed_time_ms:float = 0
-    var condition_result:bool = false
-    while !condition_result and ellapsed_time_ms / 1000.0 < _at_most_duration_seconds:
-        await Engine.get_main_loop().create_timer(_poll_delay_seconds).timeout
-        ellapsed_time_ms = Time.get_ticks_msec() - start_tick_ms
-        condition_result = predicate.call()
+    _timer = Timer.new()
+    Engine.get_main_loop().root.add_child(_timer)
+    _timer.set_one_shot(true)
+    _timer.timeout.connect(func(): _on_timer_timeout_until(predicate, start_tick_ms))
+    _timer.start(_poll_delay_seconds)
+
+    var result:Array = await _on_until_condition
+
+    var condition_result:bool = result[0]
+    var ellapsed_time_ms:float = result[1]
+
+    _timer.stop()
+    Engine.get_main_loop().root.remove_child(_timer)
+    _timer.queue_free()
+    _timer = null
 
     # Test is done (success of failure, but done) ; we remove await failed assertion
     _reporter.assertion_reports.erase(await_fail_report)
@@ -96,6 +106,33 @@ func until_signal_emitted_with_args(sig:Signal, args:Array = []) -> void:
 # Fonctions privées
 #------------------------------------------
 
+func _on_timer_timeout_until(predicate:Callable, start_tick_ms:int) -> void:
+    var ellapsed_time_ms = Time.get_ticks_msec() - start_tick_ms
+    if ellapsed_time_ms / 1000.0 < _at_most_duration_seconds:
+        var condition_result:bool = predicate.call()
+        if condition_result:
+            _on_until_condition.emit([true, ellapsed_time_ms])
+        else:
+            _timer.start(_poll_delay_seconds)
+    else:
+        _on_until_condition.emit([false, ellapsed_time_ms])
+
+func _on_timer_timeout_until_signal_emitted(signal_collector:SignalCollector, check_arguments:bool, sig:Signal, args:Array, start_tick_ms:int) -> void:
+    var ellapsed_time_ms = Time.get_ticks_msec() - start_tick_ms
+    if ellapsed_time_ms / 1000.0 < _at_most_duration_seconds:
+        var signal_received:bool = false
+        if check_arguments:
+            signal_received = signal_collector.has_received_signal_with_args(sig, args)
+        else:
+            signal_received = signal_collector.has_received_signal(sig)
+
+        if signal_received:
+            _on_until_condition.emit([true, ellapsed_time_ms])
+        else:
+            _timer.start(_poll_delay_seconds)
+    else:
+        _on_until_condition.emit([false, ellapsed_time_ms])
+
 func _until_signal_emitted(sig:Signal, check_arguments:bool = false, args:Array = []) -> void:
     # Too much args
     if args.size() > 5:
@@ -106,22 +143,20 @@ func _until_signal_emitted(sig:Signal, check_arguments:bool = false, args:Array 
     var await_fail_report:AssertionReport = _get_initial_await_failed_report()
     _reporter.assertion_reports.append(await_fail_report)
 
-    # Reset state
-    _current_tested_signal_received = false
-
-    var start_tick_ms:int = Time.get_ticks_msec()
-    var ellapsed_time_ms:float = 0
-
     var signal_collector:SignalCollector = SignalCollector.new(sig.get_object())
     signal_collector.collect_signal(sig)
 
-    while !_current_tested_signal_received and ellapsed_time_ms / 1000.0 < _at_most_duration_seconds:
-        await Engine.get_main_loop().create_timer(_poll_delay_seconds).timeout
-        ellapsed_time_ms = Time.get_ticks_msec() - start_tick_ms
-        if check_arguments:
-            _current_tested_signal_received = signal_collector.has_received_signal_with_args(sig, args)
-        else:
-            _current_tested_signal_received = signal_collector.has_received_signal(sig)
+    var start_tick_ms:int = Time.get_ticks_msec()
+    _timer = Timer.new()
+    Engine.get_main_loop().root.add_child(_timer)
+    _timer.set_one_shot(true)
+    _timer.timeout.connect(func(): _on_timer_timeout_until_signal_emitted(signal_collector, check_arguments, sig, args, start_tick_ms))
+    _timer.start(_poll_delay_seconds)
+
+    var result:Array = await _on_until_condition
+
+    var current_tested_signal_received:bool = result[0]
+    var ellapsed_time_ms:float = result[1]
 
     var success_signal_received:bool = signal_collector.has_received_signal(sig)
     signal_collector.finalize()
@@ -134,7 +169,7 @@ func _until_signal_emitted(sig:Signal, check_arguments:bool = false, args:Array 
     var message_prefix:String = "Signal '%s'" % sig.get_name() if _description.is_empty() else "'%s'" % _description
 
     # Compute report
-    if _current_tested_signal_received:
+    if current_tested_signal_received:
         if check_arguments:
             report_description = "%s received in %ss with expected arguments" % [message_prefix, ellapsed_time_s]
         else:
@@ -148,11 +183,11 @@ func _until_signal_emitted(sig:Signal, check_arguments:bool = false, args:Array 
                 report_description = "%s not received in less than %ss" % [message_prefix, _at_most_duration_seconds]
         # Signal received in time, so arguments mismatch !
         else:
-            report_description = "%s received but arguments mismatch. Expected '%s', got '%s'" % [message_prefix, args, _current_tested_signal_arguments.slice(0, args.size())]
+            report_description = "%s received but arguments mismatch. Expected '%s'" % [message_prefix, args]
 
     var report:AssertionReport = AssertionReport.new()
     report.line_number = _get_assertion_line_number()
-    report.is_success = _current_tested_signal_received
+    report.is_success = current_tested_signal_received
     report.description = report_description
     _reporter.assertion_reports.append(report)
 
